498bc68391c04e5193d8a50f3d71744b13b02c46
vlzcv <vlada.zaytseva.2009@mail.ru>
2024-07-04 21:16:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while True:
+    if my_list[index] > 0:
+        print(my_list[index])
+        index += 1
+    elif my_list[index] == 0:
+        index += 1
+        continue
+    else:
+        break
